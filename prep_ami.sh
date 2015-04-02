581d182d021ebe77cb3bda5420c3bcce0be2a748
David Lyle <dlyle65535@gmail.com>
2015-04-02 18:17:35 -0400

    Allow blueprint injection via user data.

--- prep_ami.sh
+++ prep_ami.sh
@@ -2,7 +2,11 @@
 
 shred -u /etc/ssh/*_key /etc/ssh/*_key.pub
 shred -u ~/.*history
-shred -u ~/.ssh/authorized_keys 
+shred -u ~/.ssh/authorized_keys
+shred -u /var/log/wtmp
+shred -u /var/log/btmp
+shred -u /var/log/lastlog
 
 touch /root/firstrun
 
+history -c
